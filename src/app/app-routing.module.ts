f68a89b443024eb4574fbe5da85e85b78f2fee26
UrbinaArmando <urbinaa01@gmail.com>
2021-05-31 11:10:23 -0600

    hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductoComponent } from './components/listar-producto/listar-producto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListarProductoComponent },
+  {path: 'crear-producto', component: CrearProductoComponent },
+  {path: 'editar-producto/:id', component: CrearProductoComponent },
+  {path: '**', redirectTo: '',pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
